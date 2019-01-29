045281fb037ea7eeb9ce2e829c590083430f396b
Andre Vannucci <andrelv@motorola.com>
2019-01-29 08:52:25 -0200

    http

--- app/src/main/java/com/alv/forecast/data/ApixuWeatherApiService.kt
+++ app/src/main/java/com/alv/forecast/data/ApixuWeatherApiService.kt
@@ -1,7 +1,12 @@
 package com.alv.forecast.data
 
 import com.alv.forecast.data.response.CurrentWeatherResponse
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import kotlinx.coroutines.Deferred
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -15,10 +20,31 @@ interface ApixuWeatherApiService {
         @Query(value = "lang") languageCode: String = "en"
     ) : Deferred<CurrentWeatherResponse>
 
-//    companion object {
-//        operator fun invoke(): ApixuWeatherApiService {
-//            ApixuWeatherApiService
-//        }
-//    }
+    companion object {
+        operator fun invoke(): ApixuWeatherApiService {
+            val requestInterceptor = Interceptor { chain ->
+                val url = chain.request()
+                    .url()
+                    .newBuilder()
+                    .addQueryParameter("key", API_KEY)
+                    .build()
+                val request = chain.request().newBuilder().url(url).build()
+
+                return@Interceptor chain.proceed(request)
+            }
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(requestInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("https://api.apixu.com/v1/")
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ApixuWeatherApiService::class.java)
+        }
+    }
 }
 
